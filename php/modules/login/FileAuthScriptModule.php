2f63d97dc50a277863cc1adda543f7923d69272f
Piotr Gabryjeluk <piotr@wikidot.com>
2008-08-18 14:23:03 +0000

    try to fix the infinite reload problem with a logged user that is not a member of a private site

--- php/modules/login/FileAuthScriptModule.php
+++ php/modules/login/FileAuthScriptModule.php
@@ -27,7 +27,9 @@ class FileAuthScriptModule extends SmartyModule {
 	
 	public function build($runData){
 		$site = $runData->getTemp('site');
-		if($runData->getUser() && $site->getPrivate()){
+		// TODO: ControllerUtils
+		$u = new UploadedFileFlowController();
+		if($runData->getUser() && $site->getPrivate() && $u->userAllowed($runData->getUser(), $site)){
 			$pwdomain = $site->getUnixName() . "." . GlobalProperties::$URL_UPLOAD_DOMAIN;
 			$pwproto = ($_SERVER["HTTPS"]) ? "https" : "http";
 			$pwurl = "$pwproto://$pwdomain/filesauth.php";
