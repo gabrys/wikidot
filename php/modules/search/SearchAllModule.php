83a2e102f8dd0ed52fc1b75f64097d5da4376fe3
Piotr Gabryjeluk <piotr@wikidot.com>
2008-12-08 19:39:57 +0000

    SearchAll fix

--- php/modules/search/SearchAllModule.php
+++ php/modules/search/SearchAllModule.php
@@ -159,8 +159,8 @@ class SearchAllModule extends SmartyModule {
 		$total_pages = ceil($result_count / $perPage);
 		$pagerData = array();
 		$pagerData['current_page'] = $pageNumber;
-		$pagerData['known_pages'] = min(array($pageNumber + 2, $total_pages));
-		$pagerData['total_pages'] = $total_pages;
+		$pagerData['total_pages'] = min(array($pageNumber + 2, $total_pages));
+		$pagerData['known_pages'] = $total_pages;
 		
 		// construct URLs
 		for ($i = 0; $i < count($res); $i++) {
