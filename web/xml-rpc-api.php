5d8904ede774b031c4a96634ff2d1c06e630813a
Piotr Gabryjeluk <piotr@wikidot.com>
2009-01-22 15:55:38 +0000

    facade updates

--- web/xml-rpc-api.php
+++ web/xml-rpc-api.php
@@ -10,21 +10,42 @@ function errorHandler($errno, $errstr, $errfile, $errline) {
 	}
 	return true;
 }
-error_reporting(E_ALL | E_STRICT);
-set_error_handler('errorHandler', E_ALL | E_STRICT);
+error_reporting(E_ALL & ~E_NOTICE);
+set_error_handler('errorHandler', E_ALL & ~E_NOTICE);
 
 // authorize user
-if (! isset($_GET['key'])) {
-	header('HTTP/1.1 401 Unauthorized');
-	exit();
+if (isset($_GET['key'])) {
+	$key = $_GET['key'];
+} else {
+	$key = "";
 }
 
-if ($_GET["key"] != "thusaustuesnthao") {
-	header('HTTP/1.1 401 Unauthorized');
-	exit();
+$m = array();
+$user = null;
+$md5_hash = null;
+
+if (preg_match('|^([^:]+):([0-9a-f]{32})$|', $key, $m)) {
+	$user_name = $m[1];
+	$md5_hash = $m[2];
+	$c = new Criteria();
+	$c->add("unix_name", WDStringUtils::toUnixName($user_name));
+	$c->add("password", $md5_hash);
+	$user = DB_OzoneUserPeer::instance()->selectOne($c);
 }
 
-$user = DB_OzoneUser::instance()->selectByPrimaryKey("2");
+if (! $user) {
+	header('HTTP/1.1 401 Unauthorized');
+	header('Content-type: text/plain');
+	if ($md5_hash) {
+		echo "Login failed";
+	} else {
+		echo "You need to append ?key=<username>:<hash> to the URL, where\n\n";
+		echo " * <username> is your user unix_name (John Smith -> john-smith)\n";
+		echo " * <hash> is a md5 sum from your password\n\n";
+		echo "The URL for this example would be like: http://$_SERVER[HTTP_HOST]/xml-rpc-api.php?key=john-smith:2304d4770a72d09106045fea654c4188";
+	}
+	exit();
+}
 
 // construct facade objects
 $server = new Zend_XmlRpc_Server();
@@ -38,4 +59,5 @@ Zend_XmlRpc_Server_Fault::attachFaultException('Wikidot_Facade_Exception');
 Zend_XmlRpc_Server_Fault::attachFaultException('WDPermissionManager');
 
 // run XML-RPC server
+header("Content-type: text/xml");
 echo $server->handle();
